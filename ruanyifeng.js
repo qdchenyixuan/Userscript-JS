93ee1694ad2f495ec9d2629617623facd5869238
ClearChen <1716541500@qq.com>
2021-03-21 14:02:48 +0800

    阮一峰Blog更改字体

---
+++ ruanyifeng.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Yande.re Keyboard
+// @namespace    https://github.com/qdchenyixuan/Userscript-JS
+// @version      0.1
+// @description
+// @author       clear
+// @match        https://ruanyifeng.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const obj = document.querySelector("#container");
+    obj.style.cssText = "font-family: 'PingFang SC','Helvetica Neue','Microsoft YaHei New','STHeiti Light',sans-serif;"
+})();
